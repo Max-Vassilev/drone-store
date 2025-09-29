c77f20e09341fd071e7052f694fe34a1d577d86a
Maxim Vassilev <maximvasilev01@gmail.com>
2025-09-29 09:46:27 +0300

    Add loading temporary sign on the home page as well

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,16 +2,15 @@ import React, { useEffect, useState } from "react"
 import TripCard from "../components/TripCard"
 
 export default function Home() {
-  const [trips, setTrips] = useState([])
+  const [trips, setTrips] = useState(null)
 
   useEffect(() => {
-    async function loadTrips() {
-      const res = await fetch("http://54.89.185.135:8080/destinations") // Hardcoded EC2 IP Address
-      const data = await res.json()
-      setTrips(data)
-    }
-    loadTrips()
+    fetch("http://54.89.185.135:8080/destinations") // Hardcoded EC2 IP Address
+      .then(res => res.json())
+      .then(data => setTrips(data))
   }, [])
+
+  if (!trips) return <div className="loading-text">Loading...</div>
 
   return (
     <div className="container">
